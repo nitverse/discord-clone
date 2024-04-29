58b12fd933244a826c13e3c61526e66d0b8e617a
Nitant Makwana <nmakwana496@gmail.com>
2024-04-29 15:33:18 +0530

    added prisma and connected it with supabase

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={cn(font.className, "bg-white dark: bg-[#313338]")}>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
