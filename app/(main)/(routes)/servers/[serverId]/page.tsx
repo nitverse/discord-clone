225d398783161aa47415ff6f281f8e81a210ac81
nitverse <nmakwana496@gmail.com>
2024-06-22 18:40:43 +0530

    ChannelID page , and redirecting use to general channel

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,15 +1,49 @@
-import { FC } from 'react'
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { FC } from "react";
 
 interface pageProps {
-  
+  params: {
+    serverId: string;
+  };
 }
 
-const page: FC<pageProps> = ({}) => {
-  return (
-    <div>
-        SErver ID page
-    </div>
-)
-}
+const page: FC<pageProps> = async ({ params }) => {
+  const profile = await currentProfile();
 
-export default page
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(`/servers/${server?.id}/channels/${initialChannel?.id}`);
+};
+
+export default page;
