36546203e1d0280ec3c3af20c0fa6b391e91617b
nitverse <nmakwana496@gmail.com>
2024-06-23 16:35:00 +0530

    socket-io setup and connection

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import ModalProvider from "@/components/providers/ModalProvider";
 import { cn } from "@/lib/utils";
+import { SocketProvider } from "@/components/providers/SocketProvider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -28,8 +29,10 @@ export default function RootLayout({
             enableSystem
             storageKey="discord-theme"
           >
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
--- components/chat/ChatHeader.tsx
+++ components/chat/ChatHeader.tsx
@@ -3,6 +3,7 @@ import { FC } from "react";
 import { Hash } from "lucide-react";
 import MobileToggle from "../MobileToggle";
 import UserAvatar from "../UserAvatar";
+import { SocketIndicator } from "../SocketIndicator";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -24,11 +25,11 @@ const ChatHeader: FC<ChatHeaderProps> = ({
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
       {type === "conversation" && (
-        <UserAvatar src={imageUrl} className="h-5 w-5 md:h-5 md:w-5 mr-2"/>
+        <UserAvatar src={imageUrl} className="h-5 w-5 md:h-5 md:w-5 mr-2" />
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
       <div className="ml-auto flex items-center">
- 
+        <SocketIndicator />
       </div>
     </div>
   );
