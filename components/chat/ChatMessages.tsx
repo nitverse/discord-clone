36d656e834be82238d7a9c0c0a36cfdcbb77ec04
nitverse <nmakwana496@gmail.com>
2024-07-10 17:51:51 +0530

    Added ChatItem Component and ui for showing messages

--- components/chat/ChatMessages.tsx
+++ components/chat/ChatMessages.tsx
@@ -5,7 +5,8 @@ import ChatWelcome from "./ChatWelcome";
 import { useChatQuery } from "@/hooks/useChatQuery";
 import { Loader2, ServerCrash } from "lucide-react";
 import { group } from "console";
-
+import ChatItem from "./ChatItem";
+import { format } from "date-fns";
 interface ChatMessagesProps {
   name: string;
   member: Member;
@@ -18,6 +19,7 @@ interface ChatMessagesProps {
   type: "channel" | "conversation";
 }
 
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 type MessageWithMemberWithProfile = Message & {
   member: Member & { profile: Profile };
 };
@@ -61,7 +63,19 @@ const ChatMessages: FC<ChatMessagesProps> = ({
           {data?.pages?.map((group, i) => (
             <Fragment key={i}>
               {group.items.map((message: MessageWithMemberWithProfile) => (
-                <div key={message.id}>{message.content}</div>
+                <ChatItem
+                  key={message.id}
+                  id={message.id}
+                  currentMember={member}
+                  member={message.member}
+                  fileUrl={message.fileUrl}
+                  content={message.content}
+                  deleted={message.deleted}
+                  timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                  isUpdated={message.updatedAt !== message.createdAt}
+                  socketUrl={socketUrl}
+                  socketQuery={socketQuery}
+                />
               ))}
             </Fragment>
           ))}
